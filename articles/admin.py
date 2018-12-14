75b17b13bb67e37c5ecf8a33fcf99b598886061e
hijal <hijikeshhijal@gmail.com>
2018-12-14 22:20:26 +0600

    final_commit

--- articles/admin.py
+++ articles/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment
 
-admin.site.register(Article)
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
--- articles/views.py
+++ articles/views.py
@@ -2,24 +2,50 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from .models import Article
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin,ListView):
     model = Article
+    login_url = 'login'
     template_name = 'article_list.html'
 
-class DetailViewPage(DetailView):
+class DetailViewPage(LoginRequiredMixin, DetailView):
     model = Article
+    login_url = 'login'
     template_name = 'article_detail.html'
-class UpdateViewPage(UpdateView):
+class UpdateViewPage( LoginRequiredMixin, UpdateView):
     model = Article
+    login_url = 'login'
     template_name = 'article_edit.html'
-    fields = ['title', 'body']
-class DeleteViewPage(DeleteView):
+    fields = ('title', 'body')
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            raise PermissionDenied
+        return super().dispatch(request, *args, **kwargs)
+
+
+class DeleteViewPage(LoginRequiredMixin, DeleteView):
     model = Article
+    login_url = 'login'
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
+    
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            raise PermissionDenied
+        return super().dispatch(request, *args, **kwargs)
 
-class CreateViewPage(CreateView):
+
+class CreateViewPage(LoginRequiredMixin,CreateView):
     model = Article
     template_name ='article_new.html'
-    fields = ['title', 'body', 'author']
+    fields = ('title', 'body')
+    login_url = 'login'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
